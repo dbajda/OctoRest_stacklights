f7efd1d756dc094fa4c56397d4fe0f563f0a0904
dbajda <david.bajda@gmail.com>
2021-02-13 22:01:16 +0100

    Update run.py
    Changed variable names to English, added "printer offline" blink sequence.

--- run.py
+++ run.py
@@ -8,13 +8,13 @@ from gpiozero import LED
 from time import sleep
 
 # here you define outputs on your Raspberry pi
-rele1 = LED(17)
-rele2 = LED(22)
-rele3 = LED(10)
+relay1 = LED(17)
+relay2 = LED(22)
+relay3 = LED(10)
 
 # here you define your octopi instance and api key
 octopi = "octopi.local"
-api = "random api key"
+api = "api key"
 
 def make_client():
     try:
@@ -38,79 +38,66 @@ def get_printer_info():
         status = client.printer()['state']['flags']
 
         if printing == True and cancelling == False:
-            rele1.on()
-            rele2.on()
-            rele3.off()
+            relay1.on()
+            relay2.on()
+            relay3.off()
             sleep(1)
-            rele3.on()
+            relay3.on()
             sleep(1)
        # else:
-           # rele3.on()
+           # relay3.on()
         if cancelling == True and printing == True:
-            rele1.on()
-            rele3.on()
-            rele2.off()
+            relay1.on()
+            relay3.on()
+            relay2.off()
         #else:
-            #rele2.on()
+            #relay2.on()
         if ready == True:
-            rele1.on()
-            rele2.on()
-            rele3.off()
+            relay1.on()
+            relay2.on()
+            relay3.off()
         #else:
-           # rele3.on()
+           # relay3.on()
         if error == True:
-            rele3.on()
-            rele2.on()
-            rele1.off()
+            relay3.on()
+            relay2.on()
+            relay1.off()
         #else:
-         #   rele1.on()
+         #   relay1.on()
         if paused == True:
-            rele1.on()
-            rele3.on()
-            rele2.off()
+            relay1.on()
+            relay3.on()
+            relay2.off()
             sleep(1)
-            rele2.on()
+            relay2.on()
             sleep(1)
        
         #if operational == True:
-        #    rele2.off()
+        #    relay2.off()
         #else:
-         #   rele2.on()
+         #   relay2.on()
         if finishing == True:
-            rele2.off()
-            rele3.off()
+            relay2.off()
+            relay3.off()
             sleep(1)
-            rele3.on()
+            relay3.on()
             sleep(1)
         #else:
-           # rele3.on()
+           # relay3.on()
 
         return status
     except Exception as e:
         #print(e)
-        i=0
-
+        relay1.off()
+        relay2.off()
+        relay3.off()
+        sleep(1)
+        relay3.on()
+        sleep(1)
 def main():
     c = make_client()
     while True:
         print(get_printer_info())
-
-
-"""
-    while True:
-     if get_printer_info() == False:
-        rele2.on()
-        sleep(1)
-        rele1.on()
-        print("Waiting for printing")
-        sleep(1)
-        rele1.off()
-    else:
-        rele1.on()
-        rele2.off()
-        print("Printing")
-"""
-    
 
 if __name__ == "__main__":
     main()
